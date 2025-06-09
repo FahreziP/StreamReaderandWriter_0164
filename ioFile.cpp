f75314210734149d0c0ed3678f90779e847c24f7
FahreziP <fahrezipurwadianitra@gmail.com>
2025-06-09 14:35:22 +0700

    membuat string, objek output dan file untuk ditulisi

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,5 +4,14 @@
 using namespace std;
 
 int main(){
+    string baris;
+
+    // membuat objek output file stream
+    ofstream outfile;
+    // membuka file untuk ditulisi
+    outfile.open ("contohfileku.txt");
+
+    cout << ">= Menulis file, \'q\' keluar" << endl;
+
     
 }
