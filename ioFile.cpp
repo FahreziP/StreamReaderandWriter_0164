5c0648b897f739d73004fffa71efb783abfd5d7a
FahreziP <fahrezipurwadianitra@gmail.com>
2025-06-09 14:44:29 +0700

    membuat objek input file dan whileif untuk melakukan perulangan

--- ioFile.cpp
+++ ioFile.cpp
@@ -28,4 +28,18 @@ int main(){
 
     //membuat objek input dalam file stream
     ifstream infile;
+    // membuka file yang dituliskan
+    infile.open("Contohfilelagi.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open()){
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris)){
+            // dan tampilkan disini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        iofile.close()
+    }
 }
